d4559b86f4dced266f18f56688ef55790c9ff757
mfreyre <mfreyre@etsy.com>
2018-05-03 18:01:10 -0400

    refactor tests for concreteness

--- tests.py
+++ tests.py
@@ -7,76 +7,67 @@ from direction import  Direction
 class RoverTest(unittest.TestCase):
 
     def test_move_one_forward(self):
-        self._move_and_test_rover(Position(0, 0),
-                Direction.S,
-                ('f',),
-                Position(-1, 0))
+        direction = Direction.N
+        position = Position(0,0)
+        rover = Rover(position, direction)
+        commands = ('f')
+        rover.execute(commands)
+        self.assertEquals(Position(-1, 0), rover.position())
+        self.assertEquals(Direction.N, rover.direction())
 
     def test_move_one_forward_one_backward(self):
-        self._move_and_test_rover(Position(0,0),
-                Direction.S,
-                ('f', 'b'),
-                Position(0, 0))
+        direction = Direction.N
+        position = Position(0,0)
+        rover = Rover(position, direction)
+        commands = ('f', 'b')
+        rover.execute(commands)
+        self.assertEquals(Position(0, 0), rover.position())
+        self.assertEquals(Direction.N, rover.direction())
 
     def test_move_one_forward_two_backward_north(self):
-        self._move_and_test_rover(Position(0,0),
-                Direction.N,
-                ('f', 'b', 'b'),
-                Position(-1, 0))
-
-    def test_move_one_forward_two_backward_turn_left(self):
-        self._move_and_test_rover(Position(0,0),
-                Direction.N,
-                ('f', 'b', 'b', 'l', 'f'),
-                Position(-1, -1))
-
-    def test_move_one_forward_two_backward_turn_right(self):
-        self._move_and_test_rover(Position(0,0),
-                Direction.S,
-                ('f', 'b', 'b', 'r', 'f'),
-                Position(1, -1))
-
-    def test_move_one_forward_two_backward_turn_left(self):
-        self._move_and_test_rover(Position(0,0),
-                Direction.S,
-                ('f', 'b', 'b', 'l', 'f', 'f'),
-                Position(1, 2))
-
-    def test_move_east_and_turn_left_twice(self):
-        self._move_and_test_rover(Position(0,0),
-                Direction.E,
-                ('f', 'b', 'l', 'l', 'f', 'f'),
-                Position(0, -2))
-
-    def _move_and_test_rover(self, position, direction, movement, expected_position):
+        direction = Direction.N
+        position = Position(0,0)
         rover = Rover(position, direction)
-        rover.move(movement)
-        self.assertEquals(expected_position, rover.get_position())
+        commands = ('f', 'b', 'b')
+        rover.execute(commands)
+        self.assertEquals(Position(0, -1), rover.position())
+        self.assertEquals(Direction.N, rover.direction())
 
-    def test_move_west_on_finite_grid(self):
-        rover = Rover(Position(10, 10), Direction.W, (20, 20))
-        rover.move(('f', 'f'))
-        self.assertEquals(Position(10, 8), rover.get_position())
+    def test_move_one_forward_two_backward_turn_left(self):
+        direction = Direction.N
+        position = Position(0,0)
+        rover = Rover(position, direction)
+        commands = ('f', 'b', 'b', 'l')
+        rover.execute(commands)
+        self.assertEquals(Position(0, -1), rover.position())
+        self.assertEquals(Direction.W, rover.direction())
 
-    def test_wrapping_right_edge(self):
-        rover = Rover(Position(10, 10), Direction.E, (10, 10))
-        rover.move(('f', 'f', 'f'))
-        self.assertEquals(Position(10, -8), rover.get_position())
+    def test_turn_left_and_go_forward(self):
+        direction = Direction.N
+        position = Position(0,0)
+        rover = Rover(position, direction)
+        commands = ('l', 'f', 'f', 'f')
+        rover.execute(commands)
+        self.assertEquals(Position(-3, 0), rover.position())
+        self.assertEquals(Direction.W, rover.direction())
 
-    def test_wrapping_left_edge(self):
-        rover = Rover(Position(20, -30), Direction.W, (30, 30))
-        rover.move(('f', 'f'))
-        self.assertEquals(Position(20, 29), rover.get_position())
+    def test_turn_right_and_go_forward(self):
+        direction = Direction.N
+        position = Position(0,0)
+        rover = Rover(position, direction)
+        commands = ('r', 'f', 'f', 'f')
+        rover.execute(commands)
+        self.assertEquals(Position(0, 3), rover.position())
+        self.assertEquals(Direction.E, rover.direction())
 
-    def test_wrapping_upper_edge(self):
-        rover = Rover(Position(20, 0), Direction.N, (20, 30))
-        rover.move(('f', 'f', 'f', 'f'))
-        self.assertEquals(Position(-17, 0), rover.get_position())
-
-    def test_wrapping_lower_edge(self):
-        rover = Rover(Position(-19, 0), Direction.N, (20, 30))
-        rover.move(('b', 'b', 'b', 'b'))
-        self.assertEquals(Position(18, 0), rover.get_position())
+    def test_do_a_lot_of_silly_stuff(self):
+        direction = Direction.S
+        position = Position(0,0)
+        rover = Rover(position, direction)
+        commands = ('r', 'f', 'f', 'f', 'b', 'r')
+        rover.execute(commands)
+        self.assertEquals(Position(0, 2), rover.position())
+        self.assertEquals(Direction.N, rover.direction())
 
 if __name__ == '__main__':
     unittest.main()
