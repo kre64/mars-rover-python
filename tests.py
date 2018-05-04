c2fafa73688f634f766118658190f31caa8d75c8
mfreyre <mfreyre@etsy.com>
2018-05-04 09:56:59 -0400

    tests

--- tests.py
+++ tests.py
@@ -5,7 +5,46 @@ from position import Position
 from direction import  Direction
 
 class RoverTest(unittest.TestCase):
+    def test_west_turn_left(self):
+        direction = Direction.W
+        position =  Position(0,0)
+        rover = Rover(position, direction)
+        commands = ('l')
+        rover.execute(commands)
+        self.assertEquals(Position(0, 0), rover.get_position())
+        self.assertEquals(Direction.S, rover.get_direction())
 
+    @unittest.skip("unskip this line when you are ready for the next test")
+    def test_west_turn_right(self):
+        direction = Direction.W
+        position =  Position(0,0)
+        rover = Rover(position, direction)
+        commands = ('r')
+        rover.execute(commands)
+        self.assertEquals(Position(0, 0), rover.get_position())
+        self.assertEquals(Direction.N, rover.get_direction())
+
+    @unittest.skip("unskip this line when you are ready for the next test")
+    def test_east_turn_left(self):
+        direction = Direction.E
+        position =  Position(0,0)
+        rover = Rover(position, direction)
+        commands = ('l')
+        rover.execute(commands)
+        self.assertEquals(Position(0, 0), rover.get_position())
+        self.assertEquals(Direction.N, rover.get_direction())
+
+    @unittest.skip("unskip this line when you are ready for the next test")
+    def test_east_turn_right(self):
+        direction = Direction.E
+        position =  Position(0,0)
+        rover = Rover(position, direction)
+        commands = ('r')
+        rover.execute(commands)
+        self.assertEquals(Position(0, 0), rover.get_position())
+        self.assertEquals(Direction.S, rover.get_direction())
+
+    @unittest.skip("unskip this line when you are ready for the next test")
     def test_move_one_forward(self):
         direction = Direction.N
         position =  Position(0,0)
@@ -15,6 +54,7 @@ class RoverTest(unittest.TestCase):
         self.assertEquals(Position(0, 1), rover.get_position())
         self.assertEquals(Direction.N, rover.get_direction())
 
+    @unittest.skip("unskip this line when you are ready for the next test")
     def test_move_one_forward_one_backward(self):
         direction = Direction.N
         position = Position(0,0)
@@ -24,6 +64,7 @@ class RoverTest(unittest.TestCase):
         self.assertEquals(Position(0, 0), rover.get_position())
         self.assertEquals(Direction.N, rover.get_direction())
 
+    @unittest.skip("unskip this line when you are ready for the next test")
     def test_move_one_forward_two_backward_north(self):
         direction = Direction.N
         position = Position(0,0)
@@ -33,6 +74,7 @@ class RoverTest(unittest.TestCase):
         self.assertEquals(Position(0, -1), rover.get_position())
         self.assertEquals(Direction.N, rover.get_direction())
 
+    @unittest.skip("unskip this line when you are ready for the next test")
     def test_move_one_forward_two_backward_turn_left(self):
         direction = Direction.N
         position = Position(0,0)
@@ -42,6 +84,7 @@ class RoverTest(unittest.TestCase):
         self.assertEquals(Position(0, -1), rover.get_position())
         self.assertEquals(Direction.W, rover.get_direction())
 
+    @unittest.skip("unskip this line when you are ready for the next test")
     def test_turn_left_and_go_forward(self):
         direction = Direction.N
         position = Position(0,0)
@@ -51,6 +94,7 @@ class RoverTest(unittest.TestCase):
         self.assertEquals(Position(-3, 0), rover.get_position())
         self.assertEquals(Direction.W, rover.get_direction())
 
+    @unittest.skip("unskip this line when you are ready for the next test")
     def test_turn_right_and_go_forward(self):
         direction = Direction.N
         position = Position(0,0)
@@ -60,6 +104,7 @@ class RoverTest(unittest.TestCase):
         self.assertEquals(Position(3, 0), rover.get_position())
         self.assertEquals(Direction.E, rover.get_direction())
 
+    @unittest.skip("unskip this line when you are ready for the next test")
     def test_do_a_lot_of_silly_stuff(self):
         direction = Direction.S
         position = Position(0,0)
