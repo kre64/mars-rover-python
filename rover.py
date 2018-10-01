407d5e4aca08355c17f7da93324f8a8ee06c4646
kre64 <kre9404@gmail.com>
2018-10-01 18:47:04 -0400

    setup skeletons for updating the rover

--- rover.py
+++ rover.py
@@ -1,10 +1,32 @@
+from position import Position
+from direction import Direction
+
+
 class Rover:
-    def __init__(self, p, d):
-        self.position = p
-        self.direction = d
+    def __init__(self, Position, Direction):
+        self.position = Position
+        self.direction = Direction
 
     def get_position(self):
         return self.position
 
+    def get_direction(self):
+        return self.direction
+
+    def update_direction(self, command):
+        self.direction = Direction.turn(self.direction, command)
+        return True
+
+    def update_position(self, command):
+        # finish this
+        return True
+
     def execute(self, commands):
-        print("implement me!")
+        # can I make this any better?
+        for command in commands:
+            if command == 'l' or 'r':
+                self.update_direction(command)
+            elif command == 'f' or 'b':
+                self.update_direction(command)
+            else:
+                print("Invalid command.")
