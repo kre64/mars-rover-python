2bdff5fe355ceca67fa629c2bddb183bd563b9ed
kre64 <kre9404@gmail.com>
2018-10-05 17:49:23 -0400

    unskipped the skipped tests

--- tests.py
+++ tests.py
@@ -91,7 +91,7 @@ class RoverTest(unittest.TestCase):
         self.assertEquals(Position(0, -1), rover.get_position())
         self.assertEquals(Direction.W, rover.get_direction())
 
-    @unittest.skip("unskip this line when you are ready for the next test")
+    # unittest.skip("unskip this line when you are ready for the next test")
     def test_turn_left_and_go_forward(self):
         direction = Direction.N
         position = Position(0,0)
@@ -101,7 +101,7 @@ class RoverTest(unittest.TestCase):
         self.assertEquals(Position(-3, 0), rover.get_position())
         self.assertEquals(Direction.W, rover.get_direction())
 
-    @unittest.skip("unskip this line when you are ready for the next test")
+    # unittest.skip("unskip this line when you are ready for the next test")
     def test_turn_right_and_go_forward(self):
         direction = Direction.N
         position = Position(0,0)
@@ -111,7 +111,7 @@ class RoverTest(unittest.TestCase):
         self.assertEquals(Position(3, 0), rover.get_position())
         self.assertEquals(Direction.E, rover.get_direction())
 
-    @unittest.skip("unskip this line when you are ready for the next test")
+    # unittest.skip("unskip this line when you are ready for the next test")
     def test_do_a_lot_of_silly_stuff(self):
         direction = Direction.S
         position = Position(0,0)
